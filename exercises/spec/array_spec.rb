4d42b4779b0ba729bb334881ecf3798da804253f
App Academy Student <student@appacademy.io>
2015-07-03 11:18:04 -0700

    finished writing specs

--- exercises/spec/array_spec.rb
+++ exercises/spec/array_spec.rb
@@ -18,14 +18,79 @@ describe Array do
     end
   end
 
-  subject(:median_arr) { [1, 2, 3, 4, 5, 6, 7] }
+  subject(:median_arr_odd) { [1, 2, 3, 4, 5, 6, 7] }
+  subject(:median_arr_even) { [1, 2, 3, 4, 5, 6, 7, 8] }
   describe "#median" do
     it "finds the median of a given array of integers" do
-      expect(median_arr.median).to eq(4)
+      expect(median_arr_odd.median).to eq(4)
+      expect(median_arr_even.median).to eq(4.5)
     end
-
   end
 
+  subject(:matrix) do
+    [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]
+  end
+  subject(:transposed_matrix) do
+    [
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8]
+    ]
+  end
+  describe "#my_transpose" do
+    it "converts between row-oriented and column-oriented matrices" do
+      expect(matrix.my_transpose).to eq(transposed_matrix)
+    end
+  end
+end
 
+describe String do
+  subject(:zany) { "zany" }
+  subject(:hello) { "hello" }
+  describe "#caesar" do
+    it "" do
+      expect(hello.caesar(3)).to eq("khoor")
+    end
 
+    it "is careful of the letters at the end of the alphabet" do
+      expect(zany.caesar(2)).to eq("bcpa")
+    end
+  end
+end
+
+describe Hash do
+  subject(:hash_one) { { a: "alpha", b: "beta" } }
+  subject(:hash_two) { { b: "bravo", c: "charlie" } }
+  describe "#difference" do
+    it "returns a new hash containing only the keys that appear in one or the other of the hashes (but not both!)" do
+      expect(hash_one.difference(hash_two)).to eq({ a: "alpha", c: "charlie" })
+    end
+  end
+end
+
+describe Fixnum do
+  describe "#stringify" do
+    it "converts to base 10" do
+      expect(5.stringify(10)).to eq("5")
+      expect(234.stringify(10)).to eq("234")
+    end
+
+    it "converts to base 2" do
+      expect(5.stringify(2)).to eq("101")
+      expect(234.stringify(2)).to eq("11101010")
+    end
+
+    it "converts to base 16" do
+      expect(234.stringify(2)).to eq("11101010")
+      expect(234.stringify(16)).to eq("EA")
+    end
+
+    it "doen't call built in to_s"
+      expect(5.stringify(10)).not_to recieve(:to_s)
+    end
+  end
 end
