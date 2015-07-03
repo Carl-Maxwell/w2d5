12aa74c77a43e47837320171955d54d8a46c5def
App Academy Student <student@appacademy.io>
2015-07-03 14:38:44 -0700

    complete deck specs

--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -20,16 +20,41 @@ describe Deck do
     end
   end
 
+  describe "#shuffle" do
+    it "calls the array#shuffle method" do
+      # cards = fresh_deck.send(:cards)
+      # cards = fresh_deck.instance_variable_get(:@cards)
+
+      expect(fresh_deck.cards).to receive(:shuffle)
+
+      fresh_deck.shuffle
+    end
+  end
+
   describe "#take" do
     it "takes n number of cards from the deck" do
       partial_deck.take(2)
       expect(partial_deck.count).to be(0)
     end
 
+    subject(:returned_cards) { partial_deck.take(1) }
     it "takes the correct cards from top of the deck" do
-      subject(:returned_cards) { partial_deck.take(1) }
       expect(returned_cards[0].value).to eq(:ace)
     end
   end
 
+  describe "#return_cards" do
+    it "puts cards back in the bottom of the deck" do
+      cards = partial_deck.take(2)
+      partial_deck.return_cards(cards)
+      expect(partial_deck.length).to eq(2)
+    end
+
+    it "puts cards back in the bottom of the deck" do
+      cards = partial_deck.take(2)
+      partial_deck.return_cards(cards)
+      partial_deck.return_cards(cards)
+      expect(partial_deck.length).to eq(4)
+    end
+  end
 end
